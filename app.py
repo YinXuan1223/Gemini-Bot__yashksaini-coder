af62ac7a55f599f6a2b8c7483bfe495285f5a891
YinXuan1223 <leeyinxuan.cs12@nycu.edu.tw>
2025-07-31 16:44:15 +0800

    update: Migrate to the new Google GenAI SDK(text to text part)

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, redirect, render_template, url_for, request, jsonify
 from dotenv import load_dotenv
-import google.generativeai as genai
+from google import genai
 from PIL import Image
 import os
 from markdown import markdown
 
 load_dotenv('.env')
-genai.configure(api_key=os.getenv('API_KEY'))
+client = genai.Client(api_key=os.getenv('API_KEY'))
+chat_model = client.chats.create(model='gemini-2.5-flash')
 
-model = genai.GenerativeModel('gemini-pro')
-chat_model = model.start_chat(history=[])   # chat based on history
-
-img_model = genai.GenerativeModel('gemini-pro-vision')
 
 app = Flask(__name__)
 
@@ -21,17 +18,17 @@ def home():
     return render_template("index.html")
 
 # Text to text
-
-
 @app.route("/chat", methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
         query = request.json['query']
+        print(f'query:{query}')
         if (len(query.strip()) == 0):
             return jsonify("Please enter something!")
         try:
             gemini_response = chat_model.send_message(
-                query).text   # Send message based on the chat history
+                query).text  # Send message based on the chat history
+            print(f'gemini_response: {gemini_response}')
         except:
             return jsonify("Something went wrong!")
 
@@ -40,23 +37,22 @@ def chat():
         return render_template("chats.html")
 
 # Image to text
-
-
 @app.route("/image_chat", methods=['POST', 'GET'])
 def image_chat():
-    if request.method == 'POST':
-        img = request.files['image']   # Loads the file
-        q = request.form['query']   # Loads the query
+    
+    # if request.method == 'POST':
+    #     img = request.files['image']   # Loads the file
+    #     q = request.form['query']   # Loads the query
 
-        image = Image.open(img)   # Read the image in PIL format
-        try:
-            response = img_model.generate_content(
-                [q, image])   # Generate content for the image
-        except:  # noqa: E722
-            return jsonify("Something went wrong!")
-        return jsonify(markdown(response.text))
-    else:
-        return render_template("image_upload.html")
+    #     image = Image.open(img)   # Read the image in PIL format
+    #     try:
+    #         response = img_model.generate_content(
+    #             [q, image])   # Generate content for the image
+    #     except:  # noqa: E722
+    #         return jsonify("Something went wrong!")
+    #     return jsonify(markdown(response.text))
+    # else:
+         return render_template("image_upload.html")
 
 
 if __name__ == "__main__":
